3cc3b81e087122253fdf95a65b365c1267b723b3
Enohense <williamssandra803@gmail.com>
2025-09-07 07:31:01 +0100

    Permission

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -80,17 +80,15 @@ def save_user_profile(sender, instance, **kwargs):
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(
-        Author,
-        on_delete=models.CASCADE,
-        related_name="books"
-    )
+        Author, on_delete=models.CASCADE, related_name="books")
 
     def __str__(self):
         return f"{self.title} ({self.author.name})"
 
+    # <-- add this block exactly
     class Meta:
         permissions = (
-            ("canaddbook", "Can add book"),
-            ("canchangebook", "Can change book"),
-            ("candeletebook", "Can delete book"),
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
         )
