7c661147c7715a6e5ec88b8ed81579e5c8e55ee2
Enohense <williamssandra803@gmail.com>
2025-09-07 06:38:27 +0100

    auth app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
 
-app_name = "relationship_app"
 
 urlpatterns = [
     path("books/", list_books, name="list-books"),                          # FBV
@@ -10,5 +9,5 @@ urlpatterns = [
 
     path("login/",  LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
-    path("register/", register, name="register"),
+    path("register/", views.register, name="register"),
 ]
