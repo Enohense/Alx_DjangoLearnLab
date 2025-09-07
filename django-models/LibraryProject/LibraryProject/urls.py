ad5a5c8996f33c8e8661d236093f798928a8428b
Enohense <williamssandra803@gmail.com>
2025-09-07 05:53:29 +0100

    new view

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include
 from django.contrib import admin
 from django.urls import path
 
@@ -21,9 +22,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('relationship_app.urls')),  # <-- add this line
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+# relationship_app/views.py
+from django.http import HttpResponse
+from django.views.generic import DetailView
+from .models import Book, Library
 
-# Create your views here.
+
+# --- Function-based view: list all books as plain text ---
+def list_books(request):
+    """
+    Returns a simple text list of all books with their authors.
+    """
+    books = Book.objects.select_related("author").order_by("title")
+    lines = [f"{b.title} by {b.author.name}" for b in books]
+    return HttpResponse("\n".join(lines), content_type="text/plain")
+
+
+# --- Class-based view: details of a specific library (with books) ---
+class LibraryDetailView(DetailView):
+    """
+    Shows details for a single Library and its books.
+    Uses a template: relationship_app/library_detail.html
+    """
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
+
+    # pull books and their authors efficiently
+    def get_queryset(self):
+        return (
+            super()
+            .get_queryset()
+            .prefetch_related("books__author")
+        )
